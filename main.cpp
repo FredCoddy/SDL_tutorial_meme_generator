86aed719f92758718d79657e01324cc3a7173afa
coddy33 <nanouwow@hotmail.fr>
2018-11-16 17:08:20 +0100

    update cpp Homemade version

--- main.cpp
+++ main.cpp
@@ -3,41 +3,9 @@
 
 using namespace std; 
 
-int main(int argc, char **argv)
-{	
-    // Notre fenêtre
-	
-    SDL_Window* fenetre(0);
+void main_loop(){
     SDL_Event evenements;
     bool terminer(false);
-	
-	
-    // Initialisation de la SDL
-	
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << std::endl;
-        SDL_Quit();
-		
-        return -1;
-    }
-	
-	
-    // Création de la fenêtre
-
-    fenetre = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
-    if(fenetre == 0)
-    {
-        std::cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << std::endl;
-        SDL_Quit();
-
-        return -1;
-    }
-	
-	
-    // Boucle principale
-	
     while(!terminer)
     {
 	SDL_WaitEvent(&evenements);
@@ -45,12 +13,18 @@ int main(int argc, char **argv)
 	if(evenements.window.event == SDL_WINDOWEVENT_CLOSE)
 	    terminer = true;
     }
-	
-	
-    // On quitte la SDL
-	
-    SDL_DestroyWindow(fenetre);
+}
+
+int main(int argc, char **argv)
+{	
+    SDL_Window* fenetre(0);
+    SDL_Init(SDL_INIT_VIDEO);
+    
+
+    fenetre = SDL_CreateWindow("Test Hello World SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    main_loop();
     SDL_Quit();
 	
-    return 0;
+    return EXIT_SUCCESS;
 }
+
