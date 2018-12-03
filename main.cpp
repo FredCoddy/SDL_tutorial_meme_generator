caafdc96ae37f831af32a147055dbbafc989be0a
coddy33 <nanouwow@hotmail.fr>
2018-12-04 00:27:55 +0100

    clean up code

--- main.cpp
+++ main.cpp
@@ -10,43 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-
-	// variable declarations
 	SDL_Window *win = NULL;
 	SDL_Renderer *renderer = NULL;
 	SDL_Texture *img = NULL;
-	int w, h; // texture width & height
-	int scale = 6;
-	int currentTime, previousTime;
+	SDL_Color white = {255, 255, 255};
+	int currentTime = 0, previousTime = 0;
 
-	// Initialize SDL.
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		return 1;
-
+	SDL_Init(SDL_INIT_VIDEO);
 	TTF_Init();
 
-	win = SDL_CreateWindow("Cours inversé test TTF",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH,HEIGHT, 0);
+	win = SDL_CreateWindow("Cours inversé test TTF", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
 	renderer = SDL_CreateRenderer(win, -1, 0);
 
-	img = IMG_LoadTexture(renderer, argv[1]);
-	SDL_QueryTexture(img, NULL, NULL, &w, &h); // get the width and height of the texture
-											   // put the location where we want the texture to be drawn into a rectangle
-											   // I'm also scaling the texture 2x simply by setting the width and height
-											   // SDL_Rect texr; texr.x = 0; texr.y = 0; texr.w= w/scale; texr.h = h/scale;
+	img = IMG_LoadTexture(renderer, IMG_PATH);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	TTF_Font *font = TTF_OpenFont("./fonts/impact.ttf", 30);
-	const char *error = TTF_GetError();
-	SDL_Color white = {255, 255, 255};
 	SDL_Surface *surface = TTF_RenderText_Solid(font, "QUAND TU SAIS PAS QUOI DONNER COMME EXO", white);
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-
 
 	int texW = 200;
 	int texH = 200;
 	SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
-	
-	std::cout<< surface->h <<std::endl;
-	SDL_Rect dstrect = {WIDTH/2 -  surface->w/2, 25, texW, texH};
+
+	SDL_Rect dstrect = {WIDTH / 2 - surface->w / 2, 25, texW, texH};
 
 	while (1)
 	{
@@ -61,12 +47,14 @@ int main(int argc, char *argv[])
 
 		currentTime = SDL_GetTicks();
 
-		if (currentTime - previousTime > 30){
+		if (currentTime - previousTime > 15)
+		{
 			dstrect.y++;
 			previousTime = currentTime;
 		}
-		if (dstrect.y > HEIGHT){
-			dstrect.y =0;
+		if (dstrect.y > HEIGHT)
+		{
+			dstrect.y = 0;
 		}
 
 		SDL_RenderClear(renderer);
@@ -78,9 +66,9 @@ int main(int argc, char *argv[])
 	SDL_DestroyTexture(texture);
 	SDL_FreeSurface(surface);
 	TTF_CloseFont(font);
-	// SDL_DestroyTexture(img);
+	SDL_DestroyTexture(img);
 	SDL_DestroyRenderer(renderer);
-	// SDL_DestroyWindow(win);
+	SDL_DestroyWindow(win);
 
 	TTF_Quit();
 	SDL_Quit();
