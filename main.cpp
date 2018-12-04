a749c866f74c0199041c5b0452cde555104cf98e
coddy33 <nanouwow@hotmail.fr>
2018-12-04 13:53:28 +0100

    add main_loop function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include  <SDL2/SDL_gamecontroller.h>
 
 #define WIDTH 800
 #define HEIGHT 600
 #define IMG_PATH "img/goodEnough.jpg"
+
+void main_loop(SDL_Rect dstrect, SDL_Renderer *renderer, SDL_Texture *img, SDL_Texture *texture);
 
 int main(int argc, char *argv[])
 {
@@ -15,11 +18,12 @@ int main(int argc, char *argv[])
 	SDL_Texture *img = NULL;
 	SDL_Color white = {255, 255, 255};
 	int currentTime = 0, previousTime = 0;
+	SDL_GameController *controller = SDL_GameControllerOpen(0);
 
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
 	TTF_Init();
 
-	win = SDL_CreateWindow("Cours inversé test TTF", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+	win = SDL_CreateWindow("Cyril meme Generator !", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
 	renderer = SDL_CreateRenderer(win, -1, 0);
 
 	img = IMG_LoadTexture(renderer, IMG_PATH);
@@ -34,34 +38,7 @@ int main(int argc, char *argv[])
 
 	SDL_Rect dstrect = {WIDTH / 2 - surface->w / 2, 25, texW, texH};
 
-	while (1)
-	{
-		SDL_Event e;
-		if (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT)
-				break;
-			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
-				break;
-		}
-
-		currentTime = SDL_GetTicks();
-
-		if (currentTime - previousTime > 15)
-		{
-			dstrect.y++;
-			previousTime = currentTime;
-		}
-		if (dstrect.y > HEIGHT)
-		{
-			dstrect.y = 0;
-		}
-
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, img, NULL, NULL);
-		SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-		SDL_RenderPresent(renderer);
-	}
+	main_loop(dstrect, renderer, img, texture);
 
 	SDL_DestroyTexture(texture);
 	SDL_FreeSurface(surface);
@@ -74,3 +51,53 @@ int main(int argc, char *argv[])
 	SDL_Quit();
 	return 0;
 }
+
+	void main_loop(SDL_Rect dstrect, SDL_Renderer *renderer, SDL_Texture *img, SDL_Texture *texture){
+			while (1)
+	{
+		SDL_Event e;
+		if (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+				break;
+			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
+				break;
+			else if (e.key.keysym.sym == SDLK_DOWN)
+			{
+				dstrect.y++;
+			}
+			else if (e.key.keysym.sym == SDLK_UP)
+			{
+				dstrect.y--;
+			}
+			else if (e.key.keysym.sym == SDLK_LEFT)
+			{
+				dstrect.x--;
+			}
+			else if (e.key.keysym.sym == SDLK_RIGHT)
+			{
+				dstrect.x++;
+			}
+			else if (e.type == SDL_CONTROLLERBUTTONDOWN){
+				std::cout<<"coucou"<<std::endl;
+			}
+		}
+
+		// currentTime = SDL_GetTicks();
+
+		// if (currentTime - previousTime > 15)
+		// {
+		// 	dstrect.y++;
+		// 	previousTime = currentTime;
+		// }
+		// if (dstrect.y > HEIGHT)
+		// {
+		// 	dstrect.y = 0;
+		// }
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, img, NULL, NULL);
+		SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+		SDL_RenderPresent(renderer);
+	}
+	}
